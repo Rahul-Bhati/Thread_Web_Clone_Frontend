46cbd57780296aba74ed329a581aa7498e757fa9
Rahul Bhati <rb083380@gmail.com>
2024-01-19 18:32:41 +0530

    adding follw and unfollow functionlity on frontend side

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,11 +1,22 @@
-import { Avatar, Box, Flex, Menu, MenuButton, MenuItem, MenuList, Portal, Text, VStack, useToast } from "@chakra-ui/react"
+import { Avatar, Box, Button, Flex, Menu, MenuButton, MenuItem, MenuList, Portal, Text, VStack, useToast } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
 import { BsInstagram } from "react-icons/bs";
 import { CgMoreO } from "react-icons/cg";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { useState } from "react";
+import useShowToast from "../hooks/useShowToast";
 
 
 const UserHeader = ({ user }) => {
      const toast = useToast();
+     const currentUser = useRecoilValue(userAtom); // this is the current user who is logged in
+     const [following, setFollowing] = useState(user.followers.includes(currentUser._id));
+     const showToast = useShowToast();
+     const [updating, setUpdating] = useState(false); // this is for the update profile button
+
+     // console.log(user,currentUser, following, setFollowing);
+
      const copyURL = () => {
           navigator.clipboard.writeText(window.location.href).then(() => {
                toast({
@@ -16,6 +27,47 @@ const UserHeader = ({ user }) => {
                })
           })
      }
+
+     const handleFollowUnFollow = async() => {
+          if(!currentUser._id){
+               showToast("Error", "Please login to follow", 'error');
+               return;
+          }
+          setUpdating(true);
+
+          try {
+               const res = await fetch(`/api/users/follow/${user._id}`, {
+                    method:"POST",
+                    headers: {
+                         "Content-Type": "application/json",
+                    }
+               });
+               const data = await res.json();
+               
+               if (data.error){
+                    showToast("Error", data.error, 'error');
+                    return;
+               } 
+
+               
+               if(following){
+                    showToast("Success", `Unfollowed ${user.name}`, 'success');
+                    user.followers.pop();
+               }
+               else{
+                    showToast("Success", `Followed ${user.name}`, 'success');
+                    user.followers.push(currentUser._id);
+               }
+               
+               setFollowing(!following);
+          } catch (error) {
+               showToast("Error", error, 'error')
+          }
+          finally{
+               setUpdating(false);
+          }
+     }
+
      return (
           <VStack gap={4} align={"start"}>
                <Flex justifyContent={"space-between"} w={"full"}>
@@ -40,6 +92,13 @@ const UserHeader = ({ user }) => {
                </Flex>
 
                <Text>{user.bio}</Text>
+
+               {currentUser._id === user._id && (<Link to="/update" className="profile_link">
+                    <Button size={"sm"}>Update profile</Button>
+               </Link>)}
+
+               {currentUser._id !== user._id && (<Button size={"sm"} isLoading={updating} onClick={handleFollowUnFollow}>{following ? "Unfollow" : "Follow"}</Button>)}
+
                <Flex w={"full"} justifyContent={"space-between"}>
                     <Flex gap={2} alignItems={"center"}>
                          <Text color={"gray.light"}>{user.followers.length} followers</Text>
